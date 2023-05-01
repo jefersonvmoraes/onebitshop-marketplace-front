85a190731e9b0d1af38c7eb9ed4037a2b458e5f7
Jeferson <jefersonvm2@hotmail.com>
2023-05-01 15:28:44 -0300

    Criacao da lista de produtos da pagina home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import {View, Text} from "react-native";
 import Header from "../../components/common/Header";
 import NavBar from "../../components/common/NavBar";
+import ProductList from "../../components/common/ProductList";
 import { Container } from "./styled";
 
 const Home = () => {
     return (
         <Container>
             <Header/>
+            <ProductList/>
             <NavBar/>
         </Container>
     )
