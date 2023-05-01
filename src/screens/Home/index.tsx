a3532534d71f9a1e1c4348382af3bdd5323b951e
Jeferson <jefersonvm2@hotmail.com>
2023-05-01 12:37:51 -0300

    Finalizacao do header, colocando ele na pasta common

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {View, Text} from "react-native";
+import Header from "../../components/common/Header";
+import { Container } from "./styled";
+
+const Home = () => {
+    return (
+        <Container>
+            <Header/>
+        </Container>
+    )
+}
+
+export default Home;
