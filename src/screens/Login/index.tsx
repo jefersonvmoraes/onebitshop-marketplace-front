d1eb4c09e36a07413c95b53ee1fd5a0e76d3c8f2
Jeferson <jefersonvm2@hotmail.com>
2023-05-01 16:39:57 -0300

    criacao do form de login e botao default

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,14 +1,35 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Container, Logo } from './styled';
+import React from 'react';
+import { Container, ForgetPassword, Input, InputContainer, Logo } from './styled';
 import BackIcon from '../../components/common/BackIcon';
+import DefaultButton from '../../components/common/DefaultButton';
+import { Alert } from 'react-native';
 
 const logo = require("../../../assets/images/logo.png")
 const Login = () => {
+
+  const handleLogin = () => {
+    Alert.alert("Botao de login")
+  }
   return (
     <Container>
       <BackIcon marginLeft={30}/>
       <Logo source={logo}/>
+      <InputContainer>
+        <Input placeholder='Email' placeholderTextColor="white"/>
+      </InputContainer>
+
+      <InputContainer>
+        <Input placeholder='Senha' placeholderTextColor="white" secureTextEntry={true}/>
+      </InputContainer>
+      <ForgetPassword>Esqueceu sua senha?</ForgetPassword>
+      <DefaultButton 
+        buttonType='primary' 
+        marginVertical={40} 
+        buttonText="Fazer Login"
+        buttonHandle={()=>{
+          handleLogin();
+        }}
+      />
     </Container>
   )
 }
