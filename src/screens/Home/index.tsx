61948e5609717fd5578abea9b370b3f046aa1efc
Jeferson <jefersonvm2@hotmail.com>
2023-05-01 13:07:35 -0300

    Criacao da barra de navegacao

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import {View, Text} from "react-native";
 import Header from "../../components/common/Header";
+import NavBar from "../../components/common/NavBar";
 import { Container } from "./styled";
 
 const Home = () => {
     return (
         <Container>
             <Header/>
+            <NavBar/>
         </Container>
     )
 }
